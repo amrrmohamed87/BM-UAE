764cce76d70dce9702c1d40965c4e44385bced1b
Amr <frank1276smith@gmail.com>
2024-06-12 13:50:44 +0300

    add the sidebar to the RootLayout and give the main wrapper a styles to align the Outlet perfectly with the sidebar

--- src/root/RootLayout.jsx
+++ src/root/RootLayout.jsx
@@ -1,8 +1,10 @@
+import Sidebar from "@/components/Sidebar";
 import { Outlet } from "react-router-dom";
 
 export function RootLayout() {
   return (
-    <main>
+    <main className="w-full h-screen flex flex-row relative">
+      <Sidebar />
       <Outlet />
     </main>
   );
