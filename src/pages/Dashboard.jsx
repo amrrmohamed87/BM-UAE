31ebfbb7604d0fa93339fef837628596b68de7b7
Amr <frank1276smith@gmail.com>
2024-06-12 13:49:57 +0300

    giving dummy content to the dashboard page for testing purposes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,9 @@
 export function Dashboard() {
   return (
-    <div>
-      <h1>Dashboard</h1>
-    </div>
+    <section className="bg-gray-100 flex flex-col p-10 ml-20 w-full gap-5">
+      <h1 className="text-4xl text-neutral-900">Dashboard</h1>
+      <div className="w-full h-80 border border-neutral-500/50 bg-neutral-100 rounded" />
+    </section>
   );
 }
 
