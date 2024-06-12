3df954775583a2e5487d46cff31bd6bc188665e4
Amr <frank1276smith@gmail.com>
2024-06-12 11:26:04 +0300

    adding route definitions to the app component and wrap itt with RouterProvider Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Login from "./pages/Login";
-
+import Dashboard from "./pages/Dashboard";
+import RootLayout from "@/root/RootLayout";
 function App() {
-  return (
-    <>
-      <Login />
-    </>
-  );
+  const routes = createBrowserRouter([
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/",
+      element: <RootLayout />,
+      children: [
+        {
+          path: "/",
+          element: <Dashboard />,
+        },
+      ],
+    },
+  ]);
+  return <RouterProvider router={routes} />;
 }
 
 export default App;
