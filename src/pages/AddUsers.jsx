549494784f93b07259f9e1c2558e6a635a2bae6d
Amr <frank1276smith@gmail.com>
2024-06-14 13:55:27 +0300

    set up Add users component to manage users

---
+++ src/pages/AddUsers.jsx
@@ -0,0 +1,11 @@
+export function AddUsers() {
+  return (
+    <section className="bg-gray-100 flex flex-col p-10 ml-20 w-full gap-2">
+      <h1 className="text-4xl text-neutral-900">Users</h1>
+      <p className="text-[18px] text-gray-500">Manage your users</p>
+      <div className="w-full h-80 border border-neutral-500/50 bg-neutral-100 rounded" />
+    </section>
+  );
+}
+
+export default AddUsers;
