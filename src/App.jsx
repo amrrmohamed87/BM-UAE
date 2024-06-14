884bd88ca8dee5c46a6eb0476055862da0902624
Amr <frank1276smith@gmail.com>
2024-06-14 14:00:20 +0300

    adding routes to the new pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import RootLayout from "./root/RootLayout";
+import OrderEntry from "./pages/OrderEntry";
+import SupplyChain from "./pages/SupplyChain";
+import CreateOrder from "./pages/CreateOrder";
+import PurchaseOrder from "./pages/PurchaseOrder";
+import AddUsers from "./pages/AddUsers";
 function App() {
   const routes = createBrowserRouter([
     {
@@ -15,6 +20,26 @@ function App() {
         {
           path: "/",
           element: <Dashboard />,
+        },
+        {
+          path: "/order-entry",
+          element: <OrderEntry />,
+        },
+        {
+          path: "/supply-chain",
+          element: <SupplyChain />,
+        },
+        {
+          path: "/create-order",
+          element: <CreateOrder />,
+        },
+        {
+          path: "/purchase-order",
+          element: <PurchaseOrder />,
+        },
+        {
+          path: "/create-account",
+          element: <AddUsers />,
         },
       ],
     },
