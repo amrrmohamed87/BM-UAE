515a421f42aa8fe695051238538ba646cdd14969
Amr <frank1276smith@gmail.com>
2024-06-14 13:58:49 +0300

    setup Order entry component to fill the items

---
+++ src/pages/OrderEntry.jsx
@@ -0,0 +1,10 @@
+export function OrderEntry() {
+  return (
+    <section className="bg-gray-100 flex flex-col p-10 ml-20 w-full gap-5">
+      <h1 className="text-4xl text-neutral-900">Order Entry</h1>
+      <div className="w-full h-80 border border-neutral-500/50 bg-neutral-100 rounded" />
+    </section>
+  );
+}
+
+export default OrderEntry;
