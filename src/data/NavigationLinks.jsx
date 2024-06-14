c7690bcc5d874390e45a24b1d5e8b20b7495d8ff
Amr <frank1276smith@gmail.com>
2024-06-14 13:53:16 +0300

    testing a new styles for the navigation links

--- src/data/NavigationLinks.jsx
+++ src/data/NavigationLinks.jsx
@@ -4,7 +4,11 @@ export function NavigationLinks({ link, name, children }) {
   return (
     <NavLink
       to={link}
-      className="p-1 rounded flex stroke-[0.75] hover:stroke-neutral-100 stroke-neutral-400 text-neutral-400 hover:text-neutral-100 place-items-center gap-3 hover:bg-neutral-700/30 transition-color duration-100"
+      className={({ isActive }) =>
+        isActive
+          ? "bg-blue-900 p-1 rounded flex stroke-[0.75] hover:stroke-neutral-100 stroke-neutral-100 text-neutral-100 hover:text-neutral-100 place-items-center gap-3 hover:bg-blue-900 transition-color duration-300"
+          : "bg-transarent p-1 rounded flex stroke-[0.75] hover:stroke-neutral-100 stroke-neutral-400 text-neutral-400 hover:text-neutral-100 place-items-center gap-3 hover:bg-blue-900 transition-color duration-300"
+      }
     >
       {children}
       <p className="text-inherit overflow-clip whitespace-nowrap tracking-wide">
@@ -15,3 +19,20 @@ export function NavigationLinks({ link, name, children }) {
 }
 
 export default NavigationLinks;
+
+/**
+ * 
+ * <NavLink
+      to={link}
+      className={({ isActive }) =>
+        isActive
+          ? "bg-neutral-700/30 p-1 rounded flex stroke-[0.75] hover:stroke-neutral-100 stroke-neutral-400 text-neutral-400 hover:text-neutral-100 place-items-center gap-3 hover:bg-neutral-700/30 transition-color duration-100"
+          : "bg-transarent p-1 rounded flex stroke-[0.75] hover:stroke-neutral-100 stroke-neutral-400 text-neutral-400 hover:text-neutral-100 place-items-center gap-3 hover:bg-neutral-700/30 transition-color duration-100"
+      }
+    >
+      {children}
+      <p className="text-inherit overflow-clip whitespace-nowrap tracking-wide">
+        {name}
+      </p>
+    </NavLink>
+ */
