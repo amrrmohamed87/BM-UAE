5b8bfbb05405d32324c1222b6d7695b0785afd1b
Amr <frank1276smith@gmail.com>
2024-06-14 13:56:04 +0300

    setup Create order component to for form work purposes

---
+++ src/pages/CreateOrder.jsx
@@ -0,0 +1,10 @@
+export function CreateOrder() {
+  return (
+    <section className="bg-gray-100 flex flex-col p-10 ml-20 w-full gap-5">
+      <h1 className="text-4xl text-neutral-900">Create Order</h1>
+      <div className="w-full h-80 border border-neutral-500/50 bg-neutral-100 rounded" />
+    </section>
+  );
+}
+
+export default CreateOrder;
