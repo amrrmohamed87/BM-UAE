1fe20f3c8315e72461029ae0842d8cf19fa80846
Amr <frank1276smith@gmail.com>
2024-06-14 13:57:18 +0300

    Changing the bg color to test the color

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,6 @@
 export function Dashboard() {
   return (
-    <section className="bg-gray-100 flex flex-col p-10 ml-20 w-full gap-5">
+    <section className="bg-[#F5F5F5] flex flex-col p-10 ml-20 w-full gap-5">
       <h1 className="text-4xl text-neutral-900">Dashboard</h1>
       <div className="w-full h-80 border border-neutral-500/50 bg-neutral-100 rounded" />
     </section>
