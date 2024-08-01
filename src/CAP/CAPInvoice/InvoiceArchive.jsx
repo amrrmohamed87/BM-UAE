26935df56b63f8789878bd98cf22a617bbac49e2
Amr <frank1276smith@gmail.com>
2024-08-01 22:31:44 +0300

    setup both Invoice archive and review components

--- src/CAP/CAPInvoice/InvoiceArchive.jsx
+++ src/CAP/CAPInvoice/InvoiceArchive.jsx
@@ -1,8 +1,209 @@
+import { useCAPInvoice } from "@/hooks/useCAPInvoice";
+import { ToastContainer, toast } from "react-toastify";
+import { motion, AnimatePresence } from "framer-motion";
+import { PageHeader } from "@/components/PageHeader";
+import {
+  ChevronDown,
+  Download,
+  FileMinus2,
+  FileText,
+  ListChecks,
+  Loader,
+} from "lucide-react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
 export function InvoiceArchive() {
+  //? Custom hook for Invoices arhcive
+
+  const {
+    archivedInvoices,
+    isLoadingArchivedInvoices,
+    isRestoringInvoice,
+    setIsRestoringInvoice,
+    reloadTable,
+    setReloadTable,
+    selectedRows,
+    setSelectedRows,
+    filterInvoices,
+    setFilterInvoices,
+    isDeletingInvoice,
+    setIsDeletingInvoice,
+  } = useCAPInvoice("invoicesArchive");
+
   return (
     <section className="bg-[#F5F5F5] flex flex-col p-10 ml-20 w-full gap-5">
-      <h1 className="text-4xl text-neutral-900">InvoiceArchive</h1>
-      <div className="w-full h-80 border border-neutral-500/50 bg-neutral-100 rounded" />
+      <PageHeader
+        title="CAP Invoices Archive"
+        subTitle="Review all archived cap invoices and restore, delete, or extract data them through the following table."
+        data={archivedInvoices}
+        isLoadingState={isLoadingArchivedInvoices}
+        cardOneTitle="Total Archived Invoices"
+        iconOne={<FileMinus2 size={20} className="text-blue-700" />}
+        cardOneTextLink="Review CAP Invoices"
+        cardOneLink="/CAP-invoice-review"
+        cardTwoTitle="New CAP Invoice"
+        cardTwoTextLink="New Invoice"
+        iconTwo={<ListChecks size={20} className="text-blue-700" />}
+        cardTwoLink="/CAP-confirmation-review"
+      />
+
+      <div className="flex flex-col w-full my-6 bg-white border shadow rounded-lg p-4">
+        <div className="flex flex-col md:flex-row justify-start md:justify-between gap-3 items-center mb-4">
+          <div className="flex flex-col gap-3 items-start">
+            <Dialog>
+              <DialogTrigger asChild>
+                <button className="flex justify-center w-full items-center gap-1 px-10 py-2 bg-blue-900 text-white rounded-md transition-all duration-300 hover:bg-blue-500">
+                  DownLoad <Download size={18} />
+                </button>
+              </DialogTrigger>
+              <DialogContent className="sm:max-w-[425px]">
+                <DialogHeader>
+                  <DialogTitle>Download Data</DialogTitle>
+                  <DialogDescription className="mt-2">
+                    Please select your preferred format to download the data:
+                    Excel or PDF.
+                  </DialogDescription>
+                </DialogHeader>
+
+                <DialogFooter className="mt-4">
+                  <div className="flex flex-col justify-center gap-3 items-center md:justify-end md:flex-row">
+                    <button className="flex justify-center items-center gap-1 w-full md:w-[110px] px-3 py-2 bg-emerald-700 text-white rounded-md transition-all duration-300 hover:bg-emerald-900">
+                      Excel
+                    </button>
+                    <button className="flex justify-center items-center gap-1 w-full md:w-[110px] px-3 py-2 bg-red-700 text-white rounded-md transition-all duration-300 hover:bg-red-900">
+                      PDF
+                    </button>
+                  </div>
+                </DialogFooter>
+              </DialogContent>
+            </Dialog>
+
+            <button
+              onClick={() => {
+                setFilterInvoices(!filterInvoices);
+              }}
+              className="flex justify-center items-center gap-1 w-full md:w-[110px] px-3 py-2 bg-blue-900 text-white rounded-md transition-all duration-300 hover:bg-blue-500"
+            >
+              Filter
+              <motion.span
+                animate={{ rotate: filterInvoices ? -180 : 0 }}
+                transition={{ duration: 0.4 }}
+              >
+                <ChevronDown size={20} className="mt-1" />
+              </motion.span>
+            </button>
+          </div>
+
+          <div className="flex flex-col gap-3 items-center">
+            <AlertDialog>
+              <AlertDialogTrigger asChild>
+                <button className="px-7 py-2 bg-blue-900 rounded-md text-white transition-all duration-300 hover:bg-blue-500">
+                  {isRestoringInvoice ? (
+                    <motion.div
+                      animate={{ rotate: 360 }}
+                      transition={{
+                        repeat: Infinity,
+                        duration: 1,
+                        ease: "linear",
+                      }}
+                      style={{
+                        display: "inline-block",
+                      }}
+                    >
+                      <Loader className="text-white" />
+                    </motion.div>
+                  ) : (
+                    "Restore"
+                  )}
+                </button>
+              </AlertDialogTrigger>
+              <AlertDialogContent>
+                <AlertDialogHeader>
+                  <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                  <AlertDialogDescription>
+                    This action cannot be undone. This will restore those
+                    Invoices.
+                  </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                  <AlertDialogCancel onClick={() => setSelectedRows([])}>
+                    Cancel
+                  </AlertDialogCancel>
+                  <form method="delete">
+                    <AlertDialogAction>Confirm</AlertDialogAction>
+                  </form>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialog>
+
+            <AlertDialog>
+              <AlertDialogTrigger asChild>
+                <button className="px-8 py-2 bg-blue-900 rounded-md text-white transition-all duration-300 hover:bg-blue-500">
+                  {isDeletingInvoice ? (
+                    <motion.div
+                      animate={{ rotate: 360 }}
+                      transition={{
+                        repeat: Infinity,
+                        duration: 1,
+                        ease: "linear",
+                      }}
+                      style={{
+                        display: "inline-block",
+                      }}
+                    >
+                      <Loader className="text-white" />
+                    </motion.div>
+                  ) : (
+                    "Delete"
+                  )}
+                </button>
+              </AlertDialogTrigger>
+              <AlertDialogContent>
+                <AlertDialogHeader>
+                  <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                  <AlertDialogDescription>
+                    This action cannot be undone. This will delete those
+                    invoices.
+                  </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                  <AlertDialogCancel onClick={() => setSelectedRows([])}>
+                    Cancel
+                  </AlertDialogCancel>
+                  <form method="delete">
+                    <AlertDialogAction>Confirm</AlertDialogAction>
+                  </form>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialog>
+          </div>
+        </div>
+      </div>
+
+      <h1 className="text-center text-sm text-neutral-400">
+        @2024 ApexBuild, Benchmark - All rights reserved
+      </h1>
+      <br />
+      <ToastContainer />
     </section>
   );
 }
