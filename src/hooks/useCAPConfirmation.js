25bf2f362b3cba3ff5e451eca8b3f1a90d844209
Amr <frank1276smith@gmail.com>
2024-08-01 22:30:54 +0300

    adding new states for archived orders and fetch them. also adding swift state to manage swift input for the invoice creation

--- src/hooks/useCAPConfirmation.js
+++ src/hooks/useCAPConfirmation.js
@@ -10,13 +10,16 @@ export const useCAPConfirmation = (pageType) => {
     const [confirmedOrders, setConfirmedOreder] = useState([]);
     const [isLoadingOrders, setIsLoadingOrders] = useState(false);
 
-
     const [selectedRows, setSelectedRows] = useState([]);
+
+    const [archivedOrders, setArchivedOrders] = useState([]);
+    const [isLoadingArchivedOrders, setIsLoadingArchivedOrders] = useState(false);
 
     const [isDeletingOrder, setIsDeletingOrder] = useState(false);
     const [isArchivingOrder, setIsArchivingOrder] = useState(false);
     const [isRestoringOrder, setIsRestoringOrder] = useState(false);
 
+    const [isSwift, setIsSwift] = useState(false);
     const [isCreatingInvoice, setIsCreatingInvoice] = useState(false);
 
     const [filterOrders, setFilterOrders] = useState(false);
@@ -32,6 +35,7 @@ export const useCAPConfirmation = (pageType) => {
     /**
      * * useEfect to handle the fetch functions
      * 1- fetch confirmedOrders
+     * 2- fetch archivedOrders
      */
 
     useEffect(() => {
@@ -65,6 +69,35 @@ export const useCAPConfirmation = (pageType) => {
 
     console.log(confirmedOrders)
 
+    useEffect(() => {
+        if (pageType === 'archivedOrders') {
+            async function fetchArchivedOrders() {
+                setIsLoadingArchivedOrders(true);
+                try {
+                    const response = await fetch('https://benchmark-innovation-production.up.railway.app/api/cap-confirmation/archive');
+                    const resData = await response.json();
+
+                    if (!response.ok) {
+                        toast.error(resData.message);
+                        setIsLoadingArchivedOrders(false);
+                        return;
+                    }
+
+                    setArchivedOrders(resData.data);
+
+                    // TODO --> unique options for filtering purposes
+
+                    setIsLoadingArchivedOrders(false);
+                } catch (error) {
+                    toast.error(error.message);
+                    setIsLoadingArchivedOrders(false);
+                    return;
+                }
+            }
+            fetchArchivedOrders();
+        }
+    }, [reloadTable])
+
     return {
         confirmedOrders,
         isLoadingOrders,
@@ -80,5 +113,11 @@ export const useCAPConfirmation = (pageType) => {
         setIsArchivingOrder,
         isCreatingInvoice,
         setIsCreatingInvoice,
+        isSwift,
+        setIsSwift,
+        archivedOrders,
+        isLoadingArchivedOrders,
+        isRestoringOrder,
+        setIsRestoringOrder,
     }
 }
