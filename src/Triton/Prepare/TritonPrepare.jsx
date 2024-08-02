417dd17e8a31a1af7a32f229ef178128429c77ee
Amr <frank1276smith@gmail.com>
2024-08-02 23:31:44 +0300

    Deploying PrepareTriton pages

--- src/Triton/Prepare/TritonPrepare.jsx
+++ src/Triton/Prepare/TritonPrepare.jsx
@@ -1,6 +1,8 @@
 import { useTritonPrepare } from "@/hooks/useTritonPrepare";
 import { PageHeader } from "@/components/PageHeader";
+import { ToastContainer, toast } from "react-toastify";
 import TritonPrepareForm from "@/forms/TritonPrepareForm";
+import { BoxIcon, ListChecks } from "lucide-react";
 export function TritonPrepare() {
   //? useTritonPrepare Hook
 
@@ -15,8 +17,10 @@ export function TritonPrepare() {
         cardOneTitle="Items per Planned SHPDate"
         data={plannedSHPDateItems}
         isLoadingState={isLoadindPlannedSHPDateItems}
+        iconOne={<BoxIcon size={20} className="text-blue-700" />}
         cardOneLink="/items"
         cardOneTextLink="Items"
+        iconTwo={<ListChecks size={20} className="text-blue-700" />}
         cardTwoTitle="CAP Invoices"
         cardTwoLink="/CAP-invoice-review"
         cardTwoTextLink="Invoices"
@@ -24,6 +28,12 @@ export function TritonPrepare() {
       <div className="flex flex-col w-full my-6 p-4">
         <TritonPrepareForm />
       </div>
+
+      <h1 className="text-center text-sm text-neutral-400">
+        @2024 ApexBuild, Benchmark - All rights reserved
+      </h1>
+      <br />
+      <ToastContainer />
     </section>
   );
 }
