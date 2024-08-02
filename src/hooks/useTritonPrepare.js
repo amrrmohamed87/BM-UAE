5725afb051b14cd98c9358c7e3465c9c8e2bfd6d
Amr <frank1276smith@gmail.com>
2024-08-02 23:31:19 +0300

    useTritonPrepare custom hook

--- src/hooks/useTritonPrepare.js
+++ src/hooks/useTritonPrepare.js
@@ -9,9 +9,44 @@ export const useTritonPrepare = (pageType) => {
     const [plannedSHPDateItems, setPlannedSHPDateItems] = useState([]);
     const [isLoadindPlannedSHPDateItems, setIsLoadingPlannedSHPDateItems] = useState(false);
 
+    const [tritonPreparedData, setTritonPreparedData] = useState([]);
+    const [isLoadingTritonPreparedData, setIsLoadingTritonPreparedData] = useState(false);
+
+    const [filterTritonPrepare, setFilterTritonPrepare] = useState(false);
+    const [selectedRows, setSelectedRows] = useState([]);
+    const [isDeletingTritonPrepare, setIsDeletingTritonPrepare] = useState(false);
+    const [isArchivingTritonPrepare, setIsArchivingTritonPrepare] = useState(false);
+    const [isRestoringTritonPrepare, setIsRestoringTritonPrepare] = useState(false);
+    const [reloadTable, setReloadTable] = useState(false);
+
+    const [tritonPreparedArchivedData, setTritonPreparedArchivedData] = useState([]);
+    const [isLoadingTritonPreparedArchivedData, setIsLoadingTritonPreparedArchivedData] = useState(false);
+
+
 
     return {
         plannedSHPDateItems,
-        isLoadindPlannedSHPDateItems
+        isLoadindPlannedSHPDateItems,
+        //?--------------------
+        tritonPreparedData,
+        isLoadingTritonPreparedData,
+        //?--------------------
+        tritonPreparedArchivedData,
+        isLoadingTritonPreparedArchivedData,
+        //?--------------------
+        filterTritonPrepare,
+        setFilterTritonPrepare,
+        isDeletingTritonPrepare,
+        setIsDeletingTritonPrepare,
+        isArchivingTritonPrepare,
+        setIsArchivingTritonPrepare,
+        isRestoringTritonPrepare,
+        setIsRestoringTritonPrepare,
+        reloadTable,
+        setReloadTable,
+        selectedRows,
+        setSelectedRows
+        //?---------------------
+
     }
 }
