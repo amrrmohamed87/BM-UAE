21170ed8d83e52dff6f3b0deff621882b256aaca
Amr <frank1276smith@gmail.com>
2024-08-03 11:45:24 +0300

    Adding new states to manage unique data for filtering methods, rowsPerPage, and currentPage. storing unique data for the invoice data at both invoices and archivedInvoices

--- src/hooks/useCAPInvoice.js
+++ src/hooks/useCAPInvoice.js
@@ -9,7 +9,37 @@ export const useCAPInvoice = (pageType) => {
     const [capInvoices, setCAPInvoices] = useState([]);
     const [isLoadingCAPInvoices, setIsLoadingCAPInvoices] = useState(false);
 
+    //!--------------------------------
+    const [uniqueCustomerNameOptions, setUniqueCustomerNameOptions] = useState([]);
+    const [uniqueCustomerNameQueue, setUniqueCustomerNameQueue] = useState("");
+  
+    const [uniqueCAPIDOptions, setUniqueCAPIDOptions] = useState([]);
+    const [uniqueCAPIDQueue, setUniqueCAPIDQueue] = useState("");
+    
+    const [uniqueCapInvoiceNoOptions, setUniqueCapInvoiceNoOptions] = useState([]);
+    const [uniqueCapInvoiceNoQueue, setUniqueCapInvoiceNoQueue] = useState("");
+
+    const [uniqueStatusOptions, setUniqueStatusOptions] = useState([]);
+    const [uniqueStatusQueue, setUniqueStatusQueue] = useState("");
+
+    //!--------------------------------
+
+    const [uniqueArchivedCustomerNameOptions, setUniqueArchivedCustomerNameOptions] = useState([]);
+    const [uniqueArchivedCustomerNameQueue, setUniqueArchivedCustomerNameQueue] = useState("");
+  
+    const [uniqueArchivedCAPIDOptions, setUniqueArchivedCAPIDOptions] = useState([]);
+    const [uniqueArchivedCAPIDQueue, setUniqueArchivedCAPIDQueue] = useState("");
+    
+    const [uniqueArchivedCapInvoiceNoOptions, setUniqueArchivedCapInvoiceNoOptions] = useState([]);
+    const [uniqueArchivedCapInvoiceNoQueue, setUniqueArchivedCapInvoiceNoQueue] = useState("");
+
+    const [uniqueArchivedStatusOptions, setUniqueArchivedStatusOptions] = useState([]);
+    const [uniqueArchivedStatusQueue, setUniqueArchivedStatusQueue] = useState("");
+    //!--------------------------------
+
     const [selectedRows, setSelectedRows] = useState([]);
+    const [rowsPerPage, setRowsPerPage] = useState(4);
+    const [currentPage, setCurrentPage] = useState(1);
 
     const [archivedInvoices, setArchivedInvoices] = useState([]);
     const [isLoadingArchivedInvoices, setIsLoadingArchivedInvoices] = useState(false);
@@ -43,6 +73,37 @@ export const useCAPInvoice = (pageType) => {
                     setCAPInvoices(resData.data);
 
                     // TODO --> unique options for filtering purposes
+                    const uniqueCustomerNames = [
+                        ...new Set(resData.data.flatMap((invoices) => invoices.CAPOrder.PO.PFI.map((pfi) => pfi.Customer.customerName)))
+                    ].map((name) => ({
+                        label: name,
+                        value: name
+                    }));
+                    setUniqueCustomerNameOptions(uniqueCustomerNames);
+
+                    const uniqueCapIds = [
+                        ...new Set(resData.data.flatMap((invoices) => invoices.CAPOrder.PO.PFI.map((pfi) => pfi.Customer.customerCapIdNo.toString())))
+                    ].map((cap) => ({
+                        label: cap,
+                        value: cap
+                    }));
+                    setUniqueCAPIDOptions(uniqueCapIds);
+
+                    const uniqueCapInvoiceNumbers = [
+                        ...new Set(resData.data.map((invoice) => invoice.capInvoiceNo))
+                    ].map((invoiceNumber) => ({
+                        label: invoiceNumber,
+                        value: invoiceNumber
+                    }));
+                    setUniqueCapInvoiceNoOptions(uniqueCapInvoiceNumbers);
+
+                    const uniqueStatus = [
+                        ...new Set(resData.data.map((invoice) => invoice.status))
+                    ].map((status) => ({
+                        label: status,
+                        value: status
+                    }));
+                    setUniqueStatusOptions(uniqueStatus);
 
                     setIsLoadingCAPInvoices(false);
                 } catch (error) {
@@ -54,6 +115,8 @@ export const useCAPInvoice = (pageType) => {
             fetchCAPInvoices();
         }
     }, [reloadTable]);
+
+    console.log(capInvoices)
 
     useEffect(() => {
         if (pageType === 'invoicesArchive') {
@@ -72,6 +135,37 @@ export const useCAPInvoice = (pageType) => {
                     setArchivedInvoices(resData.data);
 
                     // TODO --> unique options for filtering purposes
+                    const uniqueCustomerNames = [
+                        ...new Set(resData.data.flatMap((invoices) => invoices.CAPOrder.PO.PFI.map((pfi) => pfi.Customer.customerName)))
+                    ].map((name) => ({
+                        label: name,
+                        value: name
+                    }));
+                    setUniqueArchivedCustomerNameOptions(uniqueCustomerNames);
+
+                    const uniqueCapIds = [
+                        ...new Set(resData.data.flatMap((invoices) => invoices.CAPOrder.PO.PFI.map((pfi) => pfi.Customer.customerCapIdNo.toString())))
+                    ].map((cap) => ({
+                        label: cap,
+                        value: cap
+                    }));
+                    setUniqueArchivedCAPIDOptions(uniqueCapIds);
+
+                    const uniqueCapInvoiceNumbers = [
+                        ...new Set(resData.data.map((invoice) => invoice.capInvoiceNo))
+                    ].map((invoiceNumber) => ({
+                        label: invoiceNumber,
+                        value: invoiceNumber
+                    }));
+                    setUniqueArchivedCapInvoiceNoOptions(uniqueCapInvoiceNumbers);
+
+                    const uniqueStatus = [
+                        ...new Set(resData.data.map((invoice) => invoice.status))
+                    ].map((status) => ({
+                        label: status,
+                        value: status
+                    }));
+                    setUniqueArchivedStatusOptions(uniqueStatus);
 
                     setIsLoadingArchivedInvoices(false);
                 } catch (error) {
@@ -92,12 +186,29 @@ export const useCAPInvoice = (pageType) => {
         setReloadTable,
         selectedRows,
         setSelectedRows,
+        rowsPerPage,
+        setRowsPerPage,
+        currentPage,
+        setCurrentPage,
         filterInvoices,
         setFilterInvoices,
         isDeletingInvoice,
         setIsDeletingInvoice,
         isArchivingInvoice,
         setIsArchivingInvoice,
+        //?----------------
+        uniqueCustomerNameOptions,
+        uniqueCustomerNameQueue,
+        setUniqueCustomerNameQueue,
+        uniqueCAPIDOptions,
+        uniqueCAPIDQueue,
+        setUniqueCAPIDQueue,
+        uniqueCapInvoiceNoOptions,
+        uniqueCapInvoiceNoQueue,
+        setUniqueCapInvoiceNoQueue,
+        uniqueStatusOptions,
+        uniqueStatusQueue,
+        setUniqueStatusQueue,
         //Archive states
         archivedInvoices,
         isLoadingArchivedInvoices,
