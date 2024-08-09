943142df8512c4e993332ec780ec17e5dd566af0
Amr <frank1276smith@gmail.com>
2024-08-09 13:13:49 +0300

    removing unwanted states, adding new states, fetch saved triton data

--- src/hooks/useTritonPrepare.js
+++ src/hooks/useTritonPrepare.js
@@ -16,6 +16,13 @@ export const useTritonPrepare = (pageType) => {
 
     const [uniqueDateOptions, setUniqueDateOptions] = useState([]);
     const [uniqueDateQueue, setUniqueDateQueue] = useState('');
+    const [isSavingOrder, setIsSavingOrder] = useState(false);
+
+    const [selectedRows, setSelectedRows] = useState([]);
+    const [isDeletingTritonPrepare, setIsDeletingTritonPrepare] = useState(false);
+    const [isArchivingTritonPrepare, setIsArchivingTritonPrepare] = useState(false);
+    const [isRestoringTritonPrepare, setIsRestoringTritonPrepare] = useState(false);
+    const [reloadTable, setReloadTable] = useState(false);
 
     const [rowsPerPage, setRowsPerPage] = useState(4);
     const [currentPage, setCurrentPage] = useState(1);
@@ -26,41 +33,21 @@ export const useTritonPrepare = (pageType) => {
         masterAWB: "",
     });
 
-    const [selectedItemId, setSelectedItemId] = useState(null);
-    const [customerOptions, setCustomerOptions] = useState([]);
-
-
-    const [selectedItems, setSelectedItems] = useState([]);
-    const [selectedCustomer, setSelectedCustomer] = useState([]);
-    const [itemsData, setItemsData] = useState({
-        itemId: "",
-        unit: [],
-        customerId: "",
-        quantity: "",
-        AWB: "",
-        kitNo: "",
-        basicUnitPrice: ""
-    })
-    const [isSavingOrder, setIsSavingOrder] = useState(false);
+   const [tritonSavedData, setTritonSavedData] = useState([]);
+   const [isLoadingTritonSavedData, setIsLoadingTritonSavedData] = useState(false);
     
 
     const [tritonPreparedData, setTritonPreparedData] = useState([]);
     const [isLoadingTritonPreparedData, setIsLoadingTritonPreparedData] = useState(false);
 
     const [filterTritonPrepare, setFilterTritonPrepare] = useState(false);
-    const [selectedRows, setSelectedRows] = useState([]);
-    const [isDeletingTritonPrepare, setIsDeletingTritonPrepare] = useState(false);
-    const [isArchivingTritonPrepare, setIsArchivingTritonPrepare] = useState(false);
-    const [isRestoringTritonPrepare, setIsRestoringTritonPrepare] = useState(false);
-    const [reloadTable, setReloadTable] = useState(false);
+    
 
     const [tritonPreparedArchivedData, setTritonPreparedArchivedData] = useState([]);
     const [isLoadingTritonPreparedArchivedData, setIsLoadingTritonPreparedArchivedData] = useState(false);
 
     const date = formatDate;
-    console.log(plannedSHPDateItems)
    
-
     useEffect(() => {
         if (searchDate) {
             async function fetchItemsPerData() {
@@ -100,9 +87,39 @@ export const useTritonPrepare = (pageType) => {
             }
             fetchItemsPerData();
         }
-    }, [searchDate]);
+    }, [searchDate, reloadTable]);
 
-    console.log(uniqueDateOptions)
+    useEffect(() => {
+        async function fetchSavedTriton() {
+            setIsLoadingTritonSavedData(true);
+
+            try {
+                const response = await fetch('https://benchmark-innovation-production.up.railway.app/api/triton-saving');
+                const resData = await response.json();
+                
+                if (!response.ok) {
+                    toast.error(resData.message);
+                    setIsLoadingTritonSavedData(false);
+                    return;
+                }
+
+                setTritonSavedData(resData.data);
+
+                //TODO -- Unique data
+
+                setIsLoadingTritonSavedData(false);
+            } catch (error) {
+                toast.error(error.message);
+                setIsLoadingTritonSavedData(false);
+                return;
+            }
+        }
+        fetchSavedTriton()
+    }, [reloadTable])
+
+    console.log(tritonSavedData)
+
+    
 
     return {
         plannedSHPDate,
@@ -121,19 +138,11 @@ export const useTritonPrepare = (pageType) => {
         setCurrentPage,
         //?--------------------
         
-        selectedItems,
-        setSelectedItems,
-        itemsData,
-        setItemsData,
+        
         staticData,
         setStaticData,
       
-        selectedCustomer,
-        setSelectedCustomer,
-        selectedItemId,
-        setSelectedItemId,
-        customerOptions,
-        setCustomerOptions,
+        
         isSavingOrder,
         setIsSavingOrder,
         //?--------------------
